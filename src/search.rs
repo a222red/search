7b61fb7356d168485ee8dba6018a1bf9b06518db
a222red <73256177+a222red@users.noreply.github.com>
2022-03-27 19:00:41 -0500

    Move search functions to their own module

---
+++ src/search.rs
@@ -0,0 +1,46 @@
+use memchr::memmem::Finder;
+
+use regex::Regex;
+
+pub fn search(filename: &str, text: &String, pattern: &Finder) {
+    let mut num = 0usize;
+
+    text.lines().for_each(|line| {
+        num += 1;
+
+        if let Some(i) = pattern.find(line.as_bytes()) {
+            println!(
+                "\u{001b}[34;1m[{}:{}]:\u{001b}[0m {}",
+                filename,
+                num,
+                format!(
+                    "{}\u{001b}[32m{}\u{001b}[0m{}",
+                    &line[..i],
+                    &line[i..(i + pattern.needle().len())],
+                    &line[(i + pattern.needle().len())..]
+                ).trim()
+            );
+        }
+    });
+}
+
+pub fn search_regex(filename: &str, text: &String, pattern: &Regex) {
+    let mut num = 0usize;
+
+    text.lines().for_each(|line| {
+        num += 1;
+        if let Some(mat) = pattern.find(line) {
+            println!(
+                "\u{001b}[33m[{}:{}]:\u{001b}[0m {}",
+                filename,
+                num,
+                format!(
+                    "{}\u{001b}[32m{}\u{001b}[0m{}",
+                    &line[..(mat.start())],
+                    &line[(mat.start())..(mat.end())],
+                    &line[(mat.end())..]
+                ).trim()
+            );
+        }
+    });
+}
